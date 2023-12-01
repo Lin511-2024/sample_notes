e66d8617337db388c1a97d193606c19207159015
JoFrhwld <JoFrhwld@gmail.com>
2023-12-01 16:14:31 -0500

    add sample_script

---
+++ Week1/sample_script.py
@@ -0,0 +1,11 @@
+SECRET_MESSAGE = "This is a secret message!"
+
+def say_hello(name = "person"):
+    """This will print a hello message
+
+    Args:
+        name (str, optional): The name of the person you want to greet. 
+        Defaults to "".
+    """
+
+    print(f"Hello, {name}!")
